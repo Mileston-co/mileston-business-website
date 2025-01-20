ffa58860e51ae0b9271948131b9b81ce36bb019e
munimdev <munimzafar100@gmail.com>
2025-01-20 18:54:15 +0500

    use new grid bg

--- src/pages/home/CardFunctions.js
+++ src/pages/home/CardFunctions.js
@@ -30,39 +30,30 @@ function CardFunctions() {
 
 
     return (
-        <section className='mx-8 bg-black rounded-2xl px-10 py-10 flex flex-col items-center' style={{
-            backgroundImage: `url(/assets/images/card-functions-bg.png)`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-        }}>
-
-            <div className='flex flex-col items-center section-container gap-8 !my-0'>
-            <ScrollTriggeredAnimation>
-                <div className='flex flex-col justify-center items-center gap-8 max-w-xl mx-auto'>
-                    <Badge bgColor='bg-green-900' textColor='text-green-300 ' icon={faSun}>Features</Badge>
-                    <Title className='text-white'>Unlocking the power of Mileston Checkout</Title>
-                    <Text className='text-center'>Discover the features that makes us best in the business</Text>
-                </div>
-                </ScrollTriggeredAnimation>
-
+        <section className='relative flex flex-col items-center px-10 py-10 mx-8 bg-black rounded-2xl'>
+            <div className='absolute inset-0 w-full h-full dark:bg-grid-white/[0.2] bg-grid-black/[0.2] [mask-image:radial-gradient(ellipse_at_center,white_35%,transparent_75%)]'></div>
+            <div className='flex flex-col items-center section-container gap-8 !my-0 relative z-10'>
                 <ScrollTriggeredAnimation>
-                <div className='grid lg:grid-cols-3 gap-3'>
-                    {functions.map((func, index) => (
-                        <div key={index} className='flex flex-col items-start gap-5 bg-[#242424] rounded-2xl p-5' style={{
-                            boxShadow: "2px 2px 4px #ffffff2e, inset 1px 0px 2px #ffffff2e"
-                        }}>
-                            <FontAwesomeIcon icon={func.icon} className='text-green-300 text-5xl' />
-                            <p className='text-white text-lg'>{func.title}</p>
-                            <p className='text-gray-500 text-sm'>{func.description}</p>
-                        </div>
-                    ))}
-                </div>
+                    <div className='flex flex-col items-center justify-center max-w-xl gap-8 mx-auto'>
+                        <Badge bgColor='bg-green-900' textColor='text-green-300 ' icon={faSun}>Features</Badge>
+                        <Title className='text-white'>Unlocking the power of Mileston Checkout</Title>
+                        <Text className='text-center'>Discover the features that makes us best in the business</Text>
+                    </div>
                 </ScrollTriggeredAnimation>
-
-
+                <ScrollTriggeredAnimation>
+                    <div className='grid gap-3 lg:grid-cols-3'>
+                        {functions.map((func, index) => (
+                            <div key={index} className='flex flex-col items-start gap-5 bg-[#242424] rounded-2xl p-5' style={{
+                                boxShadow: "2px 2px 4px #ffffff2e, inset 1px 0px 2px #ffffff2e"
+                            }}>
+                                <FontAwesomeIcon icon={func.icon} className='text-5xl text-green-300' />
+                                <p className='text-lg text-white'>{func.title}</p>
+                                <p className='text-sm text-gray-500'>{func.description}</p>
+                            </div>
+                        ))}
+                    </div>
+                </ScrollTriggeredAnimation>
             </div>
-
         </section>
     )
 }
