8b1306189c7f3efe545c843cda3c24c75ab9b5cd
Tomiwa Philip <tomiwaphilip1100@gmail.com>
2025-10-04 20:28:46 +0100

    chore: update package manager to pnpm@10.16.1 and add Tawk.to script for live chat support in ClientRootLayout

--- src/components/ClientRootLayout.js
+++ src/components/ClientRootLayout.js
@@ -31,6 +31,14 @@ export default function ClientRootLayout({ children }) {
         <>
             <Script id="show-banner">
                 {`!function () {var reb2b = window.reb2b = window.reb2b || []; if (reb2b.invoked) return;reb2b.invoked = true;reb2b.methods = ["identify", "collect"]; reb2b.factory = function (method) {return function () {var args = Array.prototype.slice.call(arguments); args.unshift(method);reb2b.push(args);return reb2b;};}; for (var i = 0; i < reb2b.methods.length; i++) {var key = reb2b.methods[i];reb2b[key] = reb2b.factory(key);} reb2b.load = function (key) {var script = document.createElement("script");script.type = "text/javascript";script.async = true; script.src = "https://s3-us-west-2.amazonaws.com/b2bjsstore/b/" + key + "/E63P0H73Q2OW.js.gz"; var first = document.getElementsByTagName("script")[0]; first.parentNode.insertBefore(script, first);}; reb2b.SNIPPET_VERSION = "1.0.1";reb2b.load("E63P0H73Q2OW");}();`}
+                {`var Tawk_API=Tawk_API||{ }, Tawk_LoadStart=new Date();
+                (function(){var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/68e16db3d2df3f1952328af6/1j6o991vn';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+                })();`}
             </Script>
             <GoogleAnalytics gaId="G-L2W78EFKMN" />
             <BaseFramerAnimation>
