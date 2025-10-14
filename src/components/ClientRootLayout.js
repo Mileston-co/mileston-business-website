7bc24ecb0a64944904cf62da4a7f24437e2bbe4d
Tomiwa Philip <tomiwaphilip1100@gmail.com>
2025-10-14 16:01:10 +0100

    chore: replace Tawk.to script with Smartsupp chat integration in ClientRootLayout

--- src/components/ClientRootLayout.js
+++ src/components/ClientRootLayout.js
@@ -31,14 +31,14 @@ export default function ClientRootLayout({ children }) {
         <>
             <Script id="show-banner">
                 {`!function () {var reb2b = window.reb2b = window.reb2b || []; if (reb2b.invoked) return;reb2b.invoked = true;reb2b.methods = ["identify", "collect"]; reb2b.factory = function (method) {return function () {var args = Array.prototype.slice.call(arguments); args.unshift(method);reb2b.push(args);return reb2b;};}; for (var i = 0; i < reb2b.methods.length; i++) {var key = reb2b.methods[i];reb2b[key] = reb2b.factory(key);} reb2b.load = function (key) {var script = document.createElement("script");script.type = "text/javascript";script.async = true; script.src = "https://s3-us-west-2.amazonaws.com/b2bjsstore/b/" + key + "/E63P0H73Q2OW.js.gz"; var first = document.getElementsByTagName("script")[0]; first.parentNode.insertBefore(script, first);}; reb2b.SNIPPET_VERSION = "1.0.1";reb2b.load("E63P0H73Q2OW");}();`}
-                {`var Tawk_API=Tawk_API||{ }, Tawk_LoadStart=new Date();
-                (function(){var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-                s1.async=true;
-                s1.src='https://embed.tawk.to/68e16db3d2df3f1952328af6/1j6o991vn';
-                s1.charset='UTF-8';
-                s1.setAttribute('crossorigin','*');
-                s0.parentNode.insertBefore(s1,s0);
-                })();`}
+                {`var _smartsupp = _smartsupp || { };
+                _smartsupp.key = '7b5d19b3cc18939a0021fb4531d8ee35b73feb55';
+                window.smartsupp||(function(d) {
+	var s,c,o=smartsupp=function(){o._.push(arguments)};o._=[];
+                s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+                c.type='text/javascript';c.charset='utf-8';c.async=true;
+                c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);`}
             </Script>
             <GoogleAnalytics gaId="G-L2W78EFKMN" />
             <BaseFramerAnimation>
